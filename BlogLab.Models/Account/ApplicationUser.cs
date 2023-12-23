ce773cbdb110c912355a8af0bbd4acce14433037
Satish <dummyemail20051997@gmail.com>
2023-12-24 00:44:00 +0530

    Models project complete. Repository project created with account repository.

---
+++ BlogLab.Models/Account/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿namespace BlogLab.Models.Account
+{
+    public class ApplicationUser
+    {
+        public int ApplicationUserId { get; set; }
+        public string Username { get; set; }
+        public string Fullname { get; set; }
+        public string Eamil { get; set; }
+        public string Token { get; set; }
+
+
+    }
+}
